1360ebbb68bd71e8f9dd3354d69879e8832365a4
INELstudent <nout131EPA@yandex.ru>
2021-07-28 14:57:35 +0300

    OUTPUT func added
    Tested input fetures, added output functional. Tested on the stand

--- src/main.h
+++ src/main.h
@@ -25,6 +25,12 @@
 #define LED1_Pin PORT_Pin_9
 #define LED1_GPIO_Port MDR_PORTA
 
+#define PLC_INPUT_INIT_Pin PORT_Pin_3
+#define PLC_INPUT_INIT_Port MDR_PORTA
+
+#define PLC_OUTPUT_INIT_Pin PORT_Pin_4
+#define PLC_OUTPUT_INIT_Port MDR_PORTE
+
 #define X_PIN9_Pin PORT_Pin_3
 #define X_PIN9_GPIO_Port MDR_PORTB
 #define X_PIN10_Pin PORT_Pin_4
@@ -43,21 +49,43 @@
 #define X_PIN4_GPIO_Port MDR_PORTA
 
 #define NUMBER_PIN_IN 4
+#define NUMBER_PIN_OUT 4
+#define NUMBER_GRUP 1
 #define MAX_CNT_LED 5000
+#define FILTER_CONST 50
+#define DELAY_CONST 200
+#define IMPULSE_CNT_CH 0
+
+#define NONE 0
+#define PLC_INPUT 1
+#define PLC_OUTPUT 2
 
 void main(void);
 
-void GPIO_Init(void);
+void GPIO_Init_GENERAL(void);
+void GPIO_Init_INPUT(void);
+void GPIO_Init_OUTPUT(void);
 void Timer1_Init(void);
 void SPI2_Init(void);
 
+// Функции входов
 void Read_Pin(unsigned char *Input);
 void Test_Pin(unsigned char *Input);
 void Test_Pin_Start1();
 void Test_Pin_Stop1();
 void Test_Pin_Start2();
 void Test_Pin_Stop2();
-void Led_Pin();
+
+// Функции выходов
+void Write_VT(unsigned char *Input1, unsigned char *Input2);
+void Reset_Buf(unsigned char *Buf, unsigned char Length);
+void Set_Buf(unsigned char *Buf, unsigned char Length);
+
+void Led_Out();
+void Led_Action(unsigned char *Buf_state,unsigned char *Buf_err );
+void Led_Number(unsigned char Number);
+void Switch_update();
+void Switch_indicate();
 
 void SPI2_TransmitData(uint16_t data);
 bool SPI2_TransmitReceiveData(uint16_t data);
